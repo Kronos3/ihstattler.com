b75c0d0843c3bcb152f68b3067d8371d472d2b70
tyang98 <tony.yang98@gmail.com>
2019-04-16 12:53:50 -0400

    update models.py with Tag and Post models

--- ihstattler/main/models.py
+++ ihstattler/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Post(models.Model):
+    text = models.TextField()
+    headline = models.CharField(max_length=128)
+    authors = models.ManyToManyField(User)
+
+    published_on = models.DateField(auto_now=True)
+    tags = models.ManyToManyField(Tag)
