8d05fd44abb5bfc2619a89fa12fab319c24809a3
tyang98 <tony.yang98@gmail.com>
2019-04-21 20:05:58 -0400

    complete models

--- ihstattler/main/models.py
+++ ihstattler/main/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-from datetime import datetime
+
 
 class Category(models.Model):
     term_id = models.IntegerField()
@@ -10,9 +9,13 @@ class Category(models.Model):
 
     @staticmethod
     def parse(element):
-        category = Category(element.find("./term_id").text, element.find("./category_nicename").text, element.find("category_parent").text, element.find("./cat_name)").text)
+        category = Category(element.find("./term_id").text,
+                            element.find("./category_nicename").text,
+                            element.find("category_parent").text,
+                            element.find("./cat_name)").text)
         category.save()
         return category
+
 
 class Tag(models.Model):
     term_id = models.IntegerField()
@@ -21,9 +24,12 @@ class Tag(models.Model):
 
     @staticmethod
     def parse(element):
-        tag = Tag(element.find("./term_id").text, element.find("./tag_slug").text, element.find("./tag_name"))
+        tag = Tag(element.find("./term_id").text,
+                  element.find("./tag_slug").text,
+                  element.find("./tag_name"))
         tag.save()
         return tag
+
 
 class Term(models.Model):
     term_id = models.IntegerField()
@@ -34,49 +40,95 @@ class Term(models.Model):
 
     @staticmethod
     def parse(element):
-        term = Term(element.find("./term_id").text, element.find("./term_taxonomy").text, element.find("./term_slug").text, element.find("./term_parent").text, element.find("./term_name").text)
+        term = Term(element.find("./term_id").text,
+                    element.find("./term_taxonomy").text,
+                    element.find("./term_slug").text,
+                    element.find("./term_parent").text,
+                    element.find("./term_name").text)
         term.save()
         return term
+
 
 class ImageItem(models.Model):
     title = models.CharField(max_length=32)
     link = models.CharField(max_length=32)
-    pub_date = datetime.strftime(models.charField(max_length=32), '%m/%d/%Y') #did you want me to turn this from a string into a datefield?
-    post_id = models.IntegerField()
+    pub_date = models.DateField()
     post_date = models.DateField()
     attachment_url = models.URLField()
 
     @staticmethod
     def parse(element):
-        item = ImageItem(element.find("./title").text, element.find("./link").text, element.find("./pubDate").text, element.find("./post_id").text, element.find("./post_date").text, element.find("./attachment_url").text)
+        item = ImageItem(element.find("./title").text,
+                         element.find("./link").text,
+                         element.find("./pubDate").text,
+                         element.find("./post_id").text,
+                         element.find("./post_date").text,
+                         element.find("./attachment_url").text)
         item.save()
         return item
 
-class PostItem(models.Model):
-    """ text = models.TextField()
-    headline = models.CharField(max_length=128)
-    authors = models.ManyToManyField(User)
 
-    #published_on = models.DateField(auto_now=True)
-    tags = models.ManyToManyField(Tag)"""
+class Profile(models.Model):
+    nice_name = models.CharField(max_length=64)
+    human_name = models.CharField(max_length=64)
+
+
+class PostItem(models.Model):
 
     title = models.CharField(max_length=32)
     link = models.CharField(max_length=32)
-    pub_date = datetime.strftime(models.charField(max_length=32), '%m/%d/%Y')
-    encoded_content = models.CharField(max_length=5000) #what length do I set this to because it is an article
+    pub_date = models.DateField()
+    encoded_content = models.TextField()
     post_id = models.IntegerField()
     post_date = models.DateField()
-    #<category domain="byline" nicename="john-peterson"><![CDATA[JOHN PETERSON]]></category>
-	#<category domain="category" nicename="sports"><![CDATA[Sports]]></category>
-	#<category domain="post_tag" nicename="student-feature"><![CDATA[Student Feature]]></category>
-    #How do I parse something like this
+
+    category = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name="category_tag", null=True)
+    post_tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name="article_tag", null=True)
+    byline = models.ManyToManyField(Profile)
 
     @staticmethod
     def parse(element):
-        item = PostItem(element.find("./title").text, element.find("./link").text, element.find("./pubDate").text, element.find("./content:encoded").text, element.find("./post_id").text, element.find("./post_date").text)
+        tag = None
+        post_tag = None
+        byline = []
+        encoded_content = element.find("./content:encoded").text
+        for cat in element.findAll("./category"):
+            if cat.attrib["domain"] == "category":
+                try:
+                    tag = Tag.objects.get(nice_name=cat.attrib["nicename"])
+                except Tag.objects.model.DoesNotExist:
+                    print("Tag %s not found" % cat.attrib["nicename"])
+                    tag = None
+            if cat.attrib["domain"] == "byline":
+                try:
+                    current_byline = Profile.objects.get(nice_name=cat.attrib["nicename"])
+                except Profile.objects.model.DoesNotExist:
+                    current_byline = Profile(nice_name=cat.attrib["nicename"], human_name=cat.text)
+                    current_byline.save()
+                byline.append(current_byline)
+            if cat.attrib["domain"] == "post_tag":
+                try:
+                    post_tag = Tag.objects.get(nice_name=cat.attrib["nicename"])
+                except Tag.objects.model.DoesNotExist:
+                    print("Post Tag %s not found" % cat.attrib["nicename"])
+                    post_tag = None
+
+        for meta in element.findAll("postmeta"):
+            meta_key = meta.find("meta_key").text
+            if meta_key.startswith("_oembed_") and not meta_key.startswith("_oembed_time_"):
+                encoded_content = meta.find("meta_value").text
+
+        item = PostItem(title=element.find("./title").text,
+                        link=element.find("./link").text,
+                        pub_date=element.find("./pubDate").text,
+                        encoded_content=encoded_content,
+                        post_id=element.find("./post_id").text,
+                        post_date=element.find("./post_date").text,
+                        category=tag,
+                        post_tag=post_tag,
+                        byline=byline
+
+        )
         item.save()
         return item
-
-
-
 
